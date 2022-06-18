ae63ba6f9d21dbd88273f9fe0a3bfc4119806520
Daniel Bolarinwa <dtireni@gmaail.com>
2022-06-18 20:22:42 +0100

    updated the boiler plate code for the generator and retriever lambda functions as well the folder structure to conform to java standards

---
+++ src/retriever/Retriever.java
@@ -0,0 +1,23 @@
+// put the code for retrieving the password in here
+package retriever;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Retrieve implements RequestHandler<String, String> {
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+    // put code here
+    final Map<String, String> queryParams = event.getQueryStringParameters();
+
+    final String userName = queryParams.get("userName");
+  }
+}
